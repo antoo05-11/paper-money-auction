39bfbbf05078342d0c0436fc56ecc9b749d186b8
onlyzabao <ntgbao.235@gmail.com>
2024-03-18 13:39:44 +0700

    simple query and mutation

--- back-end/src/schema/user/mutations.js
+++ back-end/src/schema/user/mutations.js
@@ -1,4 +1,12 @@
+import User from "./db.js";
+
 export const userMutations = {
-  createUser: async (_, args) => { },
+  createUser: async (_, args) => {
+    const { user } = args;
+    const newUser = await User.create(user);
+
+    newUser.id = newUser._id.toString();
+    return newUser;
+  },
   updateUser: async (_, args) => { },
 };
--- back-end/src/schema/user/queries.js
+++ back-end/src/schema/user/queries.js
@@ -1,4 +1,13 @@
+import User from './db.js';
+
 export const userQueries = {
-  users: async (_, args) => { },
-  user: async (_, args) => { },
+  users: async (_, args) => {
+
+  },
+  user: async (_, arg) => {
+    const { id } = arg;
+    const user = await User.findById(id);
+
+    return user;
+  },
 };
