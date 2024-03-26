80f08e8e8f2bf3a35881048146224899f440da57
xpbachx <21020283@vnu.edu.vn>
2024-03-26 23:26:29 +0700

    Ađd shadcn and example

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return <div>Home Page
+    <Avatar>
+    <AvatarImage src="https://github.com/shadcn.png" />
+  <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+  </div>;
 }
