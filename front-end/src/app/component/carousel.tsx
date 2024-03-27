effa508f485514d6016534e2f9a33787ef4c9c09
Đức Anh <doducanh2411@gmail.com>
2024-03-27 22:16:43 +0700

    Add homepage

---
+++ front-end/src/app/component/carousel.tsx
@@ -0,0 +1,37 @@
+import * as React from "react"
+
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
+export default function MyCarousel() {
+    return (
+        <Carousel className="w-10/12">
+            <CarouselContent className="-ml-1">
+                {Array.from({ length: 10 }).map((_, index) => (
+                    <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/4">
+                        <div className="p-1">
+                            <Card>
+                                <CardHeader className="flex justify-center items-center">
+                                    <CardDescription className="">Thời gian đấu giá</CardDescription>
+                                    <CardTitle className="font-medium text-lg">24/11/2003 09:00:00</CardTitle>
+                                </CardHeader>
+                                <CardContent className="flex justify-center items-center ">
+                                    <Image src={"/demoimage.jpg"} alt="Image" width={300} height={300} className="rounded-md" />
+                                </CardContent>
+                                <CardContent>
+                                    <h3 className="font-semibold">Rồng đỏ Macao</h3>
+                                    <p>Giá khởi điểm: 30.000 VNĐ</p>
+                                    <Button className="mt-2 bg-highlightColor">Chi tiết</Button>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+    )
+}
---
+++ front-end/src/app/component/page-footer.tsx
@@ -0,0 +1,76 @@
+import Link from "next/link";
+
+export default function PageFooter() {
+    return (
+        <footer className="bg-footerBgColor mt-10 text-white">
+            <div className="container mx-auto px-4 lg:px-0 py-10">
+                <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
+                    <div className="col-span-1 lg:col-span-2">
+                        <h3 className="font-bold text-xl mb-2">CÔNG TY ĐÁU GIÁ HỢP DANH VUA TIỀN TỆ</h3>
+                        <p>Địa chỉ: Số 28 Đỗ Đức Dục - Mễ Trì - Nam Từ Liêm - Hà Nội</p>
+                        <p>Hotline: 096 214 27 15</p>
+                        <p>Đại diện: Đỗ Đức Anh - Chức vụ: Tổng giám đốc</p>
+                    </div>
+
+                    <div className="col-span-1 lg:col-span-1">
+                        <h3 className="font-bold text-xl mb-2">Về chúng tôi</h3>
+                        <ul>
+                            <li className="mb-1">
+                                <Link href="/">
+                                    Lịch sử hình thành
+                                </Link>
+                            </li>
+                            <li className="mb-1">
+                                <Link href="/">
+                                    Tuyển dụng
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="col-span-1 lg:col-span-1">
+                        <h3 className="font-bold text-xl mb-2">Cuộc đấu giá</h3>
+                        <ul>
+                            <li className="mb-1">
+                                <Link href="/">
+                                    Cuộc đáu giá đang diễn ra
+                                </Link>
+                            </li>
+                            <li className="mb-1">
+                                <Link href="/customer/LockupTransaction">
+                                    Cuộc đấu giá sắp diễn ra
+                                </Link>
+                            </li>
+                            <li className="mb-1">
+                                <Link href="/customer/EstimateCost">
+                                    Cuộc đấu giá đã kết thúc
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="col-span-1 lg:col-span-1">
+                        <h3 className="font-bold text-xl mb-2">Chính sách</h3>
+                        <ul>
+                            <li className="mb-1">
+                                <Link href="/customer/service/doc">
+                                    Câu hỏi thường gặp
+                                </Link>
+                            </li>
+                            <li className="mb-1">
+                                <Link href="/customer/service/goods">
+                                    Chính sách bảo mật thông tin
+                                </Link>
+                            </li>
+                            <li className="mb-1">
+                                <Link href="/customer/service/care">
+                                    Điều khoản sử dụng
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
