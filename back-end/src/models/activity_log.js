3335a6ed58bfb7a056b0924146e0482fd7dbc9f9
onlyzabao <ntgbao.235@gmail.com>
2024-03-28 23:55:22 +0700

    disable currently used schema (discuss later)

--- back-end/src/models/activity_log.js
+++ back-end/src/models/activity_log.js
@@ -1,23 +1,23 @@
-import mongoose from "mongoose";
-
-const activityLogSchema = new mongoose.Schema({
-    id: String,
-    userId: String,
-    verifyNum: Boolean,
-    verifyMail: Boolean,
-    updateAccount: Boolean,
-    makeTransaction: Boolean,
-    uploadAsset: Boolean,
-    deleteAsset: Boolean,
-    uploadPost: Boolean,
-    updatePost: Boolean,
-    deletePost: Boolean,
-    createAuction: Boolean,
-    updateAuction: Boolean,
-    deleteAuction: Boolean,
-    makeOffer: Boolean,
-}, {
-    versionKey: false
-});
-
-export const ActivityLog = mongoose.model("ActivityLog", activityLogSchema);
+// import mongoose from "mongoose";
+// 
+// const activityLogSchema = new mongoose.Schema({
+//     id: String,
+//     userId: String,
+//     verifyNum: Boolean,
+//     verifyMail: Boolean,
+//     updateAccount: Boolean,
+//     makeTransaction: Boolean,
+//     uploadAsset: Boolean,
+//     deleteAsset: Boolean,
+//     uploadPost: Boolean,
+//     updatePost: Boolean,
+//     deletePost: Boolean,
+//     createAuction: Boolean,
+//     updateAuction: Boolean,
+//     deleteAuction: Boolean,
+//     makeOffer: Boolean,
+// }, {
+//     versionKey: false
+// });
+// 
+// export const ActivityLog = mongoose.model("ActivityLog", activityLogSchema);
