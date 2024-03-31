6e6ace0c247111883f7c09dd0ba01fd7e497e274
onlyzabao <ntgbao.235@gmail.com>
2024-03-31 11:07:34 +0700

    add bidding, asset router

--- back-end/src/controllers/auction.controller.js
+++ back-end/src/controllers/auction.controller.js
@@ -2,6 +2,9 @@ export default class AuctionController {
     constructor() {}
     create_auction = async (req, res) => {};
     list_auction = async (req, res) => {};
+    list_registered_auction = async (req, res) => {};
+    list_managing_auction = async (req, res) => {};
+    list_owned_auction = async (req, res) => {};
     view_auction = async (req, res) => {};
     register = async (req, res) => {};
     list_bidder = async (req, res) => {};
