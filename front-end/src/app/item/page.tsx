35a9fc8ca5fe4629bedab78bca2ddab7dd053a5b
LockyYud <dominhduybk7@gmail.com>
2024-04-04 09:25:43 +0700

    merge

--- front-end/src/app/item/page.tsx
+++ front-end/src/app/item/page.tsx
@@ -49,28 +49,28 @@ import Link from "next/link";
 import { useSelectedLayoutSegment } from "next/navigation";
 
 // This *client* component will be imported into a blog layout
-export default function BlogNavLink({
-  slug,
-  children,
-}: {
-  slug: string;
-  children: React.ReactNode;
-}) {
-  // Navigating to `/blog/hello-world` will return 'hello-world'
-  // for the selected layout segment
-  const segment = useSelectedLayoutSegment();
-  const isActive = slug === segment;
+// export default function BlogNavLink({
+//   slug,
+//   children,
+// }: {
+//   slug: string;
+//   children: React.ReactNode;
+// }) {
+//   // Navigating to `/blog/hello-world` will return 'hello-world'
+//   // for the selected layout segment
+//   const segment = useSelectedLayoutSegment();
+//   const isActive = slug === segment;
 
-  return (
-    <Link
-      href={`/blog/${slug}`}
-      // Change style depending on whether the link is active
-      style={{ fontWeight: isActive ? "bold" : "normal" }}
-    >
-      {children}
-    </Link>
-  );
-}
+//   return (
+//     <Link
+//       href={`/blog/${slug}`}
+//       // Change style depending on whether the link is active
+//       style={{ fontWeight: isActive ? "bold" : "normal" }}
+//     >
+//       {children}
+//     </Link>
+//   );
+// }
 
 const ItemListPage = () => {
   return (
