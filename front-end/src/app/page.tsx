f84f99f2a080f832a664857cc2939672575d2ede
Đức Anh <doducanh2411@gmail.com>
2024-04-05 16:31:21 +0700

    Refactor home page

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -17,41 +17,86 @@ export default function HomePage() {
   };
 
   return (
-    <div className="bg-bgColor bg-[url(/bg.png)] bg-cover">
+    <div>
       {/* === Banner === */}
-      <div className="bg-[url(/hero-header-bg.png)] bg-cover center h-screen">
+      <div className="bg-[url(/home-1-bg-shape.png)] bg-cover center h-screen">
         <PageHeader />
+        <div>
+        </div>
         <div className="container flex flex-row h-full">
-          <div className="basis-1/2 flex-row content-center pl-4">
-            <h3 className="font-semibold text-2xl mb-1.5 text-highlightColor">
-              Chào mừng bạn đến với vua tiền tệ{" "}
-            </h3>
-            <h3 className="font-bold text-3xl mb-1.5">
-              Nền tảng đấu giá hàng đầu Việt Nam
-            </h3>
-            <h1 className="font-normal mb-1.5">
-              Tự hào là một trong những nền tảng đấu giá trực tuyến hàng đầu tại Việt Nam,
-              Vua Tiền Tệ tự hào là đơn vị tiên phong trong việc sử dụng công nghệ thông tin để tối ưu hóa hoạt động đấu giá của mình.
+          <div className="basis-1/2 flex-row content-center pl-4 relative">
+            <h3 className="font-bold mb-3 text-3xl text-highlightColor">Chào mừng bạn đến với </h3>
+            <h1 className="font-bold mb-3 text-6xl animate-typing overflow-hidden whitespace-nowrap leading-normal">VUA TIỀN TỆ</h1>
+            <h1 className="font-normal mb-3">
+              Tự hào là một trong những nền tảng đấu giá trực tuyến hàng đầu tại Việt Nam.
             </h1>
             <Button className="mt-2 bg-highlightColor" onClick={executeScroll}>
               Khám phá
             </Button>
+            <div className="absolute left-72 -bottom-1 z-10" >
+              <div className=" animate-floating3">
+                <Image src="/shape3.png" width={29} height={29} alt="" />
+              </div>
+            </div>
+            <div className="absolute -bottom-32 " >
+              <div className="animate-floating2">
+                <Image src="/shape5.png" width={29} height={29} alt="" />
+              </div>
+            </div>
+            <div className="absolute -bottom-32 right-2/3" >
+              <div className="animate-floating1">
+                <Image src="/shape6.png" width={29} height={29} alt="" />
+              </div>
+            </div>
+            <div className="absolute top-1/2 z-10" >
+              <div className="animate-floating3">
+                <Image src="/shape1.png" width={10} height={10} alt="" />
+              </div>
+            </div>
           </div>
-          <div className="basis-1/2 content-center">
-            <Image
-              src={"/hero.png"}
-              alt="Logo"
-              width={450}
-              height={450}
-              className="ml-28"
-            />
+          <div className="basis-1/2 flex items-center justify-center relative">
+            <div>
+              <div className="relative">
+                <Image
+                  src={"/hero.png"}
+                  alt="Logo"
+                  width={350}
+                  height={350}
+                  className="z-20"
+                />
+                <div className="absolute top-0 right-20 animate-bid">
+                  <Image
+                    src={"/bid.png"}
+                    alt="Logo"
+                    width={80}
+                    height={80}
+                    className="z-20"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="absolute -bottom-32 right-24" >
+              <div className="animate-floating1">
+                <Image src="/shape6.png" width={29} height={29} alt="" />
+              </div>
+              <div className="animate-floating2">
+                <Image src="/shape7.png" width={10} height={10} alt="" />
+              </div>
+              <div className="animate-floating3">
+                <Image src="/shape7.png" width={10} height={10} alt="" />
+              </div>
+            </div>
           </div>
+
         </div>
+
+
       </div>
 
       {/* === Item show === */}
       <div
-        className="flex justify-center items-center flex-col pb-10"
+        className="flex justify-center items-center flex-col pb-10 bg-[url(/home1-bg2.png)] bg-cover"
         ref={assetSectionRef}
       >
         <div className="my-6">
