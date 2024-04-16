63c99d808e4b92a70528793fd4fce3c8a6c1dbb8
Đức Anh <doducanh2411@gmail.com>
2024-04-17 00:38:25 +0700

    Add profile page

--- front-end/src/app/me/document/page.tsx
+++ front-end/src/app/me/document/page.tsx
@@ -3,13 +3,44 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { UserPlus } from 'lucide-react';
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 export default function Page() {
   return (
     <div className="container">
-      <div className="flex justify-between mb-5">
-        <div className="flex flex-row">
-        </div>
-      </div>
+      <Table>
+        <TableCaption>Danh sách khách hàng</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">STT</TableHead>
+            <TableHead>Mã khách hàng</TableHead>
+            <TableHead>Họ và tên</TableHead>
+            <TableHead>Số điện thoại</TableHead>
+            <TableHead className="text-right">Chi tiết</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>1234567890</TableCell>
+            <TableCell className="text-right">
+              <Button>Chi tiết</Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
     </div>
   );
 }
