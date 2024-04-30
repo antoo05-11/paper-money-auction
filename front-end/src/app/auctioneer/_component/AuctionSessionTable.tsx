c290b890dc5f9cb6e47da2be4916574b2acf04a3
LockyYud <dominhduybk7@gmail.com>
2024-04-30 16:58:17 +0700

    perf(auctioneer): list auctioner

--- front-end/src/app/auctioneer/_component/AuctionSessionTable.tsx
+++ front-end/src/app/auctioneer/_component/AuctionSessionTable.tsx
@@ -11,8 +11,11 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function AuctionSessionTable(list_aution: any) {
+  const route = useRouter();
+  const path_name = usePathname();
   return (
     <div className="w-full">
       <Table>
@@ -20,28 +23,36 @@ export default function AuctionSessionTable(list_aution: any) {
         <TableHeader>
           <TableRow>
             <TableHead className="w-[100px]">STT</TableHead>
-            <TableHead>Mã phiên đấu giá</TableHead>
-            <TableHead>Mã tài sản</TableHead>
-            <TableHead>Trạng thái</TableHead>
+            <TableHead>Tên tài sản</TableHead>
             <TableHead>Thời gian đăng kí</TableHead>
             <TableHead>Thời gian diễn ra</TableHead>
             <TableHead className="text-center">Chi tiết</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">1</TableCell>
-            <TableCell>MTS-LEHETC</TableCell>
-            <TableCell>10/04/2024 08:00:00</TableCell>
-            <TableCell>19/04/2024 17:00:00</TableCell>
-            <TableCell>22.623.000.000 VNĐ</TableCell>
-            <TableCell>22.623.000.000 VNĐ</TableCell>
-            <TableCell className="text-center">
-              <Button variant={"ghost"} className="text-purpleColor">
-                <Eye />
-              </Button>
-            </TableCell>
-          </TableRow>
+          {list_aution?.list_auction?.map((auction: any) => {
+            return (
+              <TableRow>
+                <TableCell className="font-medium">1</TableCell>
+                <TableCell>{auction?.asset?.name}</TableCell>
+                <TableCell>10/04/2024 08:00:00</TableCell>
+                <TableCell>19/04/2024 17:00:00</TableCell>
+                {/* <TableCell>22.623.000.000 VNĐ</TableCell> */}
+                {/* <TableCell>22.623.000.000 VNĐ</TableCell> */}
+                <TableCell className="text-center">
+                  <Button
+                    variant={"ghost"}
+                    className="text-purpleColor"
+                    onClick={() => {
+                      route.push(path_name + "/" + auction?._id);
+                    }}
+                  >
+                    <Eye />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </div>
