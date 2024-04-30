652c6fd049908b4a48fc605e0a9673952705ed05
xpbachx <21020283@vnu.edu.vn>
2024-05-01 01:04:44 +0700

    feat: protected route

--- front-end/src/lib/utils.ts
+++ front-end/src/lib/utils.ts
@@ -1,13 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { useAuth } from "./auth/useAuth"
 import { User } from "./constant/dataInterface";
-import { ROLES } from "./constant/constant";
-
+import { getCookies } from "next-client-cookies/server";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function isAdmin(user: User): boolean {
-  return true;
+export function serverRole(): any {
+  const cookies = getCookies();
+  const user = JSON.parse(cookies.get('user') ?? '');
+  return user?.role;
 }
