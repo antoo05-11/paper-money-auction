8d97be61a2de8b7a5660271e50a83bce85ec1538
antoo05-11 <antooo5113@gmail.com>
2024-05-04 21:01:43 +0700

    Front-end: Update socket server.

--- back-end/src/services/auction_session/client_test/test_socket_client_1 (customer).js
+++ back-end/src/services/auction_session/client_test/test_socket_client_1 (customer).js
@@ -6,7 +6,6 @@ socket.on('connect', () => {
     console.log('Connected to server');
 
     socket.emit('join_session', sampleAuctionToken);
-
 });
 
 socket.on('join_session_response', (response) => {
@@ -27,20 +26,20 @@ socket.on('disconnect', () => {
     console.log('Disconnected from server');
 });
 
-socket.on('socket_error', (message)=>{
-     console.log('error from server:', message);
+socket.on('socket_error', (message) => {
+    console.log('error from server:', message);
 })
 
 socket.on('start_session_response', (message) => {
-    console.log(message);
+    console.log("start_session_response: " + message);
 });
 
 socket.on('make_offer_response', (message) => {
-    console.log(message);
+    console.log("make_offer_response: " + message);
 });
 
 socket.on('biddings_update', (message) => {
-    console.log(message);
+    console.log("biddings_update: " + message);
 });
 
 const sampleAuctionToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uSWQiOiI2NjIzOTY1N2NkNjAzNGFkZDk3MTQ4NWEiLCJ1c2VySWQiOiI2NjJkYzYwODQ4YWYzNTExMDA1MjJmODUiLCJyb2xlIjoiY3VzdG9tZXIiLCJpYXQiOjE3MTQyOTIxMjYsImV4cCI6MTc0NTgyODEyNn0.tHGlHdy1weQfoqV0cfzh_RmYBdYNOmQGA4UlU1hoSKc';
--- front-end/src/app/socket.ts
+++ front-end/src/app/socket.ts
@@ -4,4 +4,4 @@ import {io} from "socket.io-client";
 import path from "path";
 
 // export const socket = io("https://paper-money-auction.onrender.com/");
-export const socket = io("http://localhost:3000/", {path: "/socket/", autoConnect: false});
+export const socket = io("", {path: "https://paper-money-auction.onrender.com/", autoConnect: false});
