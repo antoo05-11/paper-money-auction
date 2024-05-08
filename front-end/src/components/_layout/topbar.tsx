f0dd5293ceb404549f260783fe55a01ed5775042
bachpx <21020283@vnu.edu.vn>
2024-05-08 11:59:01 +0700

    feat: dark mode and fix bug

--- front-end/src/components/_layout/topbar.tsx
+++ front-end/src/components/_layout/topbar.tsx
@@ -1,40 +1,47 @@
 "use client";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { usePathname, useRouter } from "next/navigation";
 import UserMenu from "./user-menu";
+import { ModeToggle } from "./mode-toggle";
 
 export default function TopBar() {
-    const router = useRouter();
-    const pathname = usePathname();
-    const routes = pathname.split('/').filter(path => path !== '');
-    const listRoutes = routes.map((route, index) => '/' + routes.slice(0, index + 1).join('/'));
+  const router = useRouter();
+  const pathname = usePathname();
+  const routes = pathname.split("/").filter((path) => path !== "");
+  const listRoutes = routes.map(
+    (route, index) => "/" + routes.slice(0, index + 1).join("/")
+  );
 
-    return (
-        <div className="container flex justify-between items-center h-14 mb-5">
-            <Breadcrumb>
-                <BreadcrumbList>
-                    <BreadcrumbSeparator />
-                    {
-                        listRoutes.map((route, index) => (
-                            <div key={index} className="inline-flex items-center">
-                                <BreadcrumbItem className="mr-2">
-                                    <BreadcrumbLink href={route}>{route.split('/')[index + 1].charAt(0).toUpperCase() + route.split('/')[index + 1].slice(1) + ' '}</BreadcrumbLink>
-                                </BreadcrumbItem>
-                                {index != listRoutes.length - 1 && <BreadcrumbSeparator />}
-                            </div>
-
-                        ))
-                    }
-                </BreadcrumbList>
-            </Breadcrumb>
-            <UserMenu />
-        </div>
-    );
+  return (
+    <div className="container flex justify-between items-center h-14 mb-5">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbSeparator />
+          {listRoutes.map((route, index) => (
+            <div key={index} className="inline-flex items-center">
+              <BreadcrumbItem className="mr-2">
+                <BreadcrumbLink href={route}>
+                  {route.split("/")[index + 1].charAt(0).toUpperCase() +
+                    route.split("/")[index + 1].slice(1) +
+                    " "}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              {index != listRoutes.length - 1 && <BreadcrumbSeparator />}
+            </div>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="pr-4">
+        <ModeToggle />
+      </div>
+      <UserMenu />
+    </div>
+  );
 }
