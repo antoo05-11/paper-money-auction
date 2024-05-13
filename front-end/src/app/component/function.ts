a64a175871fbb3f422b8a586fed9550725fdb597
LockyYud <dominhduybk7@gmail.com>
2024-05-14 00:54:10 +0700

    fix(item)

--- front-end/src/app/component/function.ts
+++ front-end/src/app/component/function.ts
@@ -1,7 +1,7 @@
 const CompareDate = (time1: string | number, time2: string | number) => {
+  // if (typeof time1 == 'string')
   const Date1 = new Date(time1);
   const Date2 = new Date(time2);
-
   if (Date1.getTime() > Date2.getTime()) return true;
   else return false;
 };
